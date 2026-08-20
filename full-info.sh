ada10aab0908f6013a57a302a880e48f1dce512c
Anton <anton_russu@mail.ru>
2026-08-20 18:24:49 +0000

    Initial empty commit

--- full-info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Пользователь: $USER"
-echo "Текущий каталог: $PWD"
-echo "Домашний каталог: $HOME"
-echo "Информация об ОС:"
-cat /etc/os-release
-echo "Время работы системы:"
-uptime
