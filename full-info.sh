e9dfd70aaabab5dd60038f3d2e76332494a594e5
Anton <anton_russu@mail.ru>
2026-08-20 15:31:15 +0000

    Первый коммит с первым домашним заданием

---
+++ full-info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Пользователь: $USER"
+echo "Текущий каталог: $PWD"
+echo "Домашний каталог: $HOME"
+echo "Информация об ОС:"
+cat /etc/os-release
+echo "Время работы системы:"
+uptime
